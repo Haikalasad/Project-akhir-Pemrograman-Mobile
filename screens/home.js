aded8fc1c46615a7b86186dbbd1a5442305ebba1
Muhammad Haikal Fikri As'ad <138842668+Haikalasad@users.noreply.github.com>
2023-12-19 13:15:17 +0700

    Redux Setup (#9)
    * Fitur read comic done
    
    * Commit changes
    
    * Redux setup

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,8 @@
 // Home.js
-import React, {useEffect} from 'react';
-import { ScrollView } from 'react-native';
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Alert } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 import Topsection from '../components/top-section';
 import PopularCarousel from '../components/carousel';
 import PopularSection from '../components/popular';
@@ -11,20 +11,28 @@ import RecomendedtSection from '../components/RecomendedComic';
 const Home = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.profile);
+  const user = useSelector((state) => state.profile);
+  const [alertShown, setAlertShown] = useState(false);
 
   useEffect(() => {
-    if (!user) {
-      navigation.navigate("Signup");
-    }
-  }, [user]);
+    const timer = setTimeout(() => {
+      console.log('User state in useEffect:', user);
   
+      if (!user.isAuthenticated) {
+        console.log('User is not registered. Navigating to Signup.');
+        navigation.navigate('Signup');
+      }
+    }, 100); // Tambahkan waktu tunggu 100 milidetik
+    return () => clearTimeout(timer);
+  }, [user, alertShown, navigation]);
+  
+
   return (
     <ScrollView>
       <Topsection />
       <PopularCarousel />
-      <RecomendedtSection/>
-      <PopularSection/>
+      <RecomendedtSection />
+      <PopularSection />
     </ScrollView>
   );
 };
