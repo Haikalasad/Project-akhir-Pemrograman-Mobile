54c154d83aa6868406a032db3fa63e0a422aa4ae
yayukagustd <yayukagustd>
2023-12-19 13:24:31 +0700

    Commit initial changes

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import profileSlice from './profileSlice';
-import taskSlice from './taskSlice';
 
 export const store = configureStore({
     reducer: {
         profile: profileSlice,
-        task: taskSlice
+    
     },
 });
